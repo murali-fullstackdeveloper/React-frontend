b2659aa61c64ae01b4a512eff9761dd1e94bddd6
murali a <133572701+murali-fullstackdeveloper@users.noreply.github.com>
2025-05-07 16:54:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
@@ -18,6 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@services': path.resolve(__dirname, './src/services'),
       '@components': path.resolve(__dirname, './src/assets/components'),
-    }
+    },
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: ['fbalb-1845050449.us-east-1.elb.amazonaws.com']
   }
-})
+});
